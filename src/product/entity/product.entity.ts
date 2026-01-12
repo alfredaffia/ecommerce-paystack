b9e9f8130fb1e02b361caa699161229a0c831113
alfredaffia <alfredaffia478@gmail.com>
2026-01-12 23:58:25 +0100

    entity model

---
+++ src/product/entity/product.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number; 
+  
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
