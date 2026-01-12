24e782a1c87641e443ef2d2664eb6ccd6a9eeff5
alfredaffia <alfredaffia478@gmail.com>
2026-01-12 23:56:36 +0100

    product  folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
 
 
 @Module({
-  imports: [],
+  imports: [ProductModule],
   controllers: [],
   providers: [],
 })
