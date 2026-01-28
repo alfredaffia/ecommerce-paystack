ec8f0f809dfcb86b24dad679fde952f47f23e8cd
alfredaffia <alfredaffia478@gmail.com>
2026-01-28 21:41:56 +0100

    order service updated

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entity/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(
+        @InjectRepository(Order)
+        private orderRepository:Repository<Order>,
+    ){}
+    async createFromWebhook(reference:string,amount:number,email:string,status:string,productId?:number){
+        const order=  this.orderRepository.create({
+            reference,
+            amount,
+            email,
+            status:'paid',
+        })
+        return await this.orderRepository.save(order);
+    }
+}
